baef8df4ece5924c7a9b350e6b2a7608ee706f5c
Gabriel Vargas <gabriel@edmodo.com>
2020-09-14 17:45:22 -0600

    adding routers and socket reader

--- server.go
+++ server.go
@@ -11,9 +11,55 @@ import (
 "github.com/gorilla/websocket"
 )
 
+var ws_upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func read(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := ws_upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+	err = ws.WriteMessage(1, []byte("Hello Client!"))
+	if err != nil {
+		log.Println(err)
+	}
+	read(ws)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Gabriel Vargas Zero Assignment")
+}
+
 func main() {
 
 	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET")
+	router.HandleFunc("/ws", wsEndpoint)
+	go echo()
+
+	log.Fatal(http.ListenAndServe(":8844", router))
 
 }
 
