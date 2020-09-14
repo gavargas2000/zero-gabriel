f62e2fa51156042d885b9e073fae90963a18e247
Gabriel Vargas <gabriel@edmodo.com>
2020-09-14 16:54:45 -0600

    adding imports and main

--- server.go
+++ server.go
@@ -1,1 +1,23 @@
-package zero_gabriel
+package server
+
+
+import (
+"encoding/json"
+"fmt"
+"log"
+"net/http"
+
+"github.com/gorilla/mux"
+"github.com/gorilla/websocket"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+}
+
+
+
+
+
