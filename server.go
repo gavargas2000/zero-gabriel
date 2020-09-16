074c102848b14229fd18962d8814edb72426b479
Gabriel Vargas <gabriel@edmodo.com>
2020-09-16 14:37:43 -0600

    error handling

--- server.go
+++ server.go
@@ -39,6 +39,11 @@ type inputItem struct {
 	Name  string `json:"name"`
 }
 
+type errorStruct struct {
+	Code int
+	Message string
+}
+
 //main "global" vars
 var currentSession = ""
 var inputItems []inputItem
@@ -140,6 +145,23 @@ func read(conn *websocket.Conn) {
 	}
 }
 
+func formatError(w http.ResponseWriter, err error, code int){
+	var errorData errorStruct
+
+	errorData.Code = code
+	errorData.Message = err.Error()
+
+
+	log.Printf("An error accured: %v", err)
+	w.WriteHeader(code)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	marshalledContent, _ := json.MarshalIndent(errorData, "", "\t")
+	w.Write(marshalledContent)
+
+}
+
 //**** Endpoints and Views ***** ///
 
 //Websocket Endpoint that reads the data, processs it,  stores it
@@ -170,6 +192,7 @@ func returnSession(w http.ResponseWriter, r *http.Request){
 
 	//Handle not found
 	if err != nil {
+		formatError(w, err, 404)
 		return
 	}
 
